f7c3a90c0c371b3914460b6c74c01d6d9e32364b
B! <iluviums@gmail.com>
2023-12-08 22:25:34 +0700

    up

---
+++ script.js
@@ -0,0 +1,58 @@
+    document.addEventListener('DOMContentLoaded', function () {
+        const externalNotesContainerHorizontal = document.getElementById('external-notes-container-horizontal');
+        const externalNotesContainerVertical = document.getElementById('external-notes-container-vertical');
+
+        // Ganti URL dengan URL yang sesuai dengan lokasi data.json
+        const dataUrl = 'data.json';
+
+        fetch(dataUrl)
+            .then(response => response.json())
+            .then(data => {
+                // Loop melalui setiap catatan dan tambahkan ke dalam container horizontal
+                data.forEach(note => {
+                    const noteElement = document.createElement('div');
+                    noteElement.classList.add('note1');
+                    noteElement.style.backgroundColor = note.color;
+
+                    const titleElement = document.createElement('h3');
+                    titleElement.textContent = note.title;
+
+                    const contentElement = document.createElement('p');
+                    contentElement.textContent = note.notes;
+
+                    const dateElement = document.createElement('h4');
+                    dateElement.textContent = note.date;
+
+                    noteElement.appendChild(titleElement);
+                    noteElement.appendChild(contentElement);
+                    noteElement.appendChild(dateElement);
+
+                    externalNotesContainerHorizontal.appendChild(noteElement);
+                });
+
+                // Loop melalui setiap catatan dan tambahkan ke dalam container vertical
+                // Reverse the order of the notes
+                data.reverse();
+                data.forEach(note => {
+                    const noteElement = document.createElement('div');
+                    noteElement.classList.add('note');
+                    noteElement.style.backgroundColor = note.color;
+                    
+                    const titleElement = document.createElement('h3');
+                    titleElement.textContent = note.title;
+
+                    const contentElement = document.createElement('p');
+                    contentElement.textContent = note.notes;
+
+                    const dateElement = document.createElement('h4');
+                    dateElement.textContent = note.date;
+
+                    noteElement.appendChild(titleElement);
+                    noteElement.appendChild(contentElement);
+                    noteElement.appendChild(dateElement);
+
+                    externalNotesContainerVertical.appendChild(noteElement);
+                });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    });
